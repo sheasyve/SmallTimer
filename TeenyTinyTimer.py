725a6f3124dfd72f7ef5f307cda55273c6f6e7b9
= <syversonshea@gmail.com>
2024-09-14 18:37:50 -0700

    v1.2 update

--- TeenyTinyTimer.py
+++ TeenyTinyTimer.py
@@ -2,77 +2,28 @@
 # Shea Syverson 2024
 
 import os
-import tkinter as tk
-from tkinter import messagebox
-import pygame
 import sys
 import platform
-import tkinter.simpledialog as simpledialog
-from ttkthemes import ThemedTk
-from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox, ttk
 import tkinter.font as tkFont
+from ttkthemes import ThemedTk
+import pygame
+
 pygame.mixer.init()
 
 class TimerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("TeenyTinyTimer")
+
         self.style = ttk.Style()
-        self.style.configure('TButton', font=('Segoe UI', 14))  # Smaller font for buttons
-        if platform.system() == "Windows":
-            self.icon_path = self.resource_path("img/icon.ico")
-            self.root.iconbitmap(self.icon_path)
-        else:
-            self.icon_path = self.resource_path("img/icon.png")
-            self.icon = tk.PhotoImage(file=self.icon_path)
-            self.root.iconphoto(True, self.icon)
-        self.hours_var = tk.StringVar(value="00")
-        self.minutes_var = tk.StringVar(value="00")
-        self.seconds_var = tk.StringVar(value="00")
-        vcmd = (root.register(self.limit_size), '%P')
-
-        # Create frames
-        self.time_frame = tk.Frame(root, bg='#353535')
-        self.time_frame.grid(row=0, column=0, sticky='nsew')
-        self.button_frame = tk.Frame(root, bg='#353535')
-        self.button_frame.grid(row=1, column=0, sticky='nsew')
-
-        # Configure grid weights for resizing
-        self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
-        self.time_frame.columnconfigure((0, 1, 2, 3, 4), weight=1)
-        self.time_frame.rowconfigure(0, weight=1)
-        self.button_frame.columnconfigure((0, 1), weight=1)
-        self.button_frame.rowconfigure(0, weight=1)
-        # Time entries and labels
-        self.hours = ttk.Entry(self.time_frame, textvariable=self.hours_var, font=("Arial", 48), width=2, justify='center', validate='key', validatecommand=vcmd)
-        self.hours.grid(row=0, column=0, sticky='nsew')
-        self.hours.bind("<FocusIn>", self.clear_default)
-        self.hours.bind("<FocusOut>", self.reset_if_empty)
-
-        self.colon1 = tk.Label(self.time_frame, text=":", font=("Arial", 48), fg='white', bg='#353535')
-        self.colon1.grid(row=0, column=1, sticky='nsew')
-
-        self.minutes = ttk.Entry(self.time_frame, textvariable=self.minutes_var, font=("Arial", 48), width=2, justify='center', validate='key', validatecommand=vcmd)
-        self.minutes.grid(row=0, column=2, sticky='nsew')
-        self.minutes.bind("<FocusIn>", self.clear_default)
-        self.minutes.bind("<FocusOut>", self.reset_if_empty)
-
-        self.colon2 = tk.Label(self.time_frame, text=":", font=("Arial", 48), fg='white', bg='#353535')
-        self.colon2.grid(row=0, column=3, sticky='nsew')
-
-        self.seconds = ttk.Entry(self.time_frame, textvariable=self.seconds_var, font=("Arial", 48), width=2, justify='center', validate='key', validatecommand=vcmd)
-        self.seconds.grid(row=0, column=4, sticky='nsew')
-        self.seconds.bind("<FocusIn>", self.clear_default)
-        self.seconds.bind("<FocusOut>", self.reset_if_empty)
-
-        # Start and Reset buttons
+        self.style.configure('TButton', font=('Segoe UI', 14))
         self.style.configure('Custom.TButton',
-                     background='#353535',
-                     foreground='white',
-                     borderwidth=1,
-                     relief='solid')
-        #Alert Style
+                             background='#353535',
+                             foreground='white',
+                             borderwidth=1,
+                             relief='solid')
         self.style.configure('Dialog.TButton',
                              background='#353535',
                              foreground='white',
@@ -80,10 +31,69 @@ class TimerApp:
                              relief='solid')
         self.style.map('Dialog.TButton',
                        background=[('active', '#454545')])
-        self.start_button = ttk.Button(self.button_frame, text="Start", command=self.start_timer, style='Custom.TButton')
+
+        if platform.system() == "Windows":
+            self.icon_path = self.resource_path("img/icon.ico")
+            self.root.iconbitmap(self.icon_path)
+        else:
+            self.icon_path = self.resource_path("img/icon.png")
+            self.icon = tk.PhotoImage(file=self.icon_path)
+            self.root.iconphoto(True, self.icon)
+
+        self.hours_var = tk.StringVar(value="00")
+        self.minutes_var = tk.StringVar(value="00")
+        self.seconds_var = tk.StringVar(value="00")
+        vcmd = (root.register(self.limit_size), '%P')
+
+        self.time_frame = tk.Frame(root, bg='#353535')
+        self.time_frame.grid(row=0, column=0, sticky='nsew')
+        self.button_frame = tk.Frame(root, bg='#353535')
+        self.button_frame.grid(row=1, column=0, sticky='ew')
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(1, weight=0)
+
+        self.time_frame.columnconfigure(0, weight=1)
+        self.time_frame.columnconfigure(2, weight=1)
+        self.time_frame.columnconfigure(4, weight=1)
+        self.time_frame.rowconfigure(0, weight=1)
+
+        self.button_frame.columnconfigure(0, weight=1)
+        self.button_frame.columnconfigure(1, weight=1)
+
+        self.hours = ttk.Entry(self.time_frame, textvariable=self.hours_var, font=("Arial", 48),
+                               width=2, justify='center', validate='key', validatecommand=vcmd)
+        self.hours.grid(row=0, column=0, sticky='nsew')
+        self.hours.bind("<FocusIn>", self.clear_default)
+        self.hours.bind("<FocusOut>", self.reset_if_empty)
+
+        self.colon1 = tk.Label(self.time_frame, text=":", font=("Arial", 48),
+                               fg='white', bg='#353535')
+        self.colon1.grid(row=0, column=1, sticky='ns')
+
+        self.minutes = ttk.Entry(self.time_frame, textvariable=self.minutes_var, font=("Arial", 48),
+                                 width=2, justify='center', validate='key', validatecommand=vcmd)
+        self.minutes.grid(row=0, column=2, sticky='nsew')
+        self.minutes.bind("<FocusIn>", self.clear_default)
+        self.minutes.bind("<FocusOut>", self.reset_if_empty)
+
+        self.colon2 = tk.Label(self.time_frame, text=":", font=("Arial", 48),
+                               fg='white', bg='#353535')
+        self.colon2.grid(row=0, column=3, sticky='ns')
+
+        self.seconds = ttk.Entry(self.time_frame, textvariable=self.seconds_var, font=("Arial", 48),
+                                 width=2, justify='center', validate='key', validatecommand=vcmd)
+        self.seconds.grid(row=0, column=4, sticky='nsew')
+        self.seconds.bind("<FocusIn>", self.clear_default)
+        self.seconds.bind("<FocusOut>", self.reset_if_empty)
+
+        self.start_button = ttk.Button(self.button_frame, text="Start", command=self.start_timer,
+                                       style='Custom.TButton')
         self.start_button.grid(row=0, column=0, sticky='nsew')
 
-        self.reset_button = ttk.Button(self.button_frame, text="Reset", command=self.reset_timer, style='Custom.TButton')
+        self.reset_button = ttk.Button(self.button_frame, text="Reset", command=self.reset_timer,
+                                       style='Custom.TButton')
         self.reset_button.grid(row=0, column=1, sticky='nsew')
 
         self.sound = pygame.mixer.Sound(self.resource_path('sounds/end.wav'))
@@ -93,13 +103,15 @@ class TimerApp:
     def resource_path(self, relative_path):
         try:
             base_path = sys._MEIPASS
-        except AttributeError:
+        except Exception:
             base_path = os.path.dirname(__file__)  
         return os.path.join(base_path, relative_path)
 
     def show_notification(self):
         CustomDialog(self.root, title="ALERT", message="Time's up!")
-        self.stop_sound() 
+        self.stop_sound()
+        self.start_button.grid(row=0, column=0, sticky='nsew')
+        self.reset_button.grid_configure(column=1, columnspan=1, sticky='nsew')
 
     def stop_sound(self, event=None):
         if self.sound_channel.get_busy():
@@ -133,6 +145,7 @@ class TimerApp:
                 self.show_notification()
                 self.time_label.grid_forget()
                 self.pack_time_entries()
+                self.start_button.grid(row=0, column=0, sticky='nsew')
 
     def start_timer(self):
         if not self.running:
@@ -143,10 +156,16 @@ class TimerApp:
                 self.time_in_seconds = hours * 3600 + minutes * 60 + seconds
                 if self.time_in_seconds > 0:
                     self.unpack_time_entries()
-                    self.time_label = tk.Label(self.time_frame, text=f"{hours:02}:{minutes:02}:{seconds:02}", font=("Arial", 48), fg='white', bg='#353535')
+                    self.time_label = tk.Label(self.time_frame,
+                                               text=f"{hours:02}:{minutes:02}:{seconds:02}",
+                                               font=("Arial", 48), fg='white', bg='#353535')
                     self.time_label.grid(row=0, column=0, columnspan=5, sticky='nsew')
+                    for i in range(5):
+                        self.time_frame.columnconfigure(i, weight=0)
                     self.time_frame.columnconfigure(0, weight=1)
                     self.running = True
+                    self.start_button.grid_remove()
+                    self.reset_button.grid_configure(column=0, columnspan=2, sticky='nsew')
                     self.update_timer()
                 else:
                     messagebox.showerror("Invalid Input", "Please enter a positive time.")
@@ -174,16 +193,20 @@ class TimerApp:
         if hasattr(self, 'time_label'):
             self.time_label.grid_forget()
             self.pack_time_entries()
+        self.start_button.grid(row=0, column=0, sticky='nsew')
+        self.reset_button.grid_configure(column=1, columnspan=1, sticky='nsew')
 
     def pack_time_entries(self):
         self.hours.grid(row=0, column=0, sticky='nsew')
-        self.colon1.grid(row=0, column=1, sticky='nsew')
+        self.colon1.grid(row=0, column=1, sticky='ns')
         self.minutes.grid(row=0, column=2, sticky='nsew')
-        self.colon2.grid(row=0, column=3, sticky='nsew')
+        self.colon2.grid(row=0, column=3, sticky='ns')
         self.seconds.grid(row=0, column=4, sticky='nsew')
-        # Reset column weights
-        self.time_frame.columnconfigure((0, 2, 4), weight=1)
-        self.time_frame.columnconfigure((1, 3), weight=0)
+        self.time_frame.columnconfigure(0, weight=1)
+        self.time_frame.columnconfigure(2, weight=1)
+        self.time_frame.columnconfigure(4, weight=1)
+        self.time_frame.columnconfigure(1, weight=0)
+        self.time_frame.columnconfigure(3, weight=0)
 
     def unpack_time_entries(self):
         self.hours.grid_forget()
@@ -226,5 +249,5 @@ if __name__ == "__main__":
     app = TimerApp(root)
     root.mainloop()
 
-#Icon source 
-#Clock by NicholasJudy456 - uploaded on October 13, 2016, 10:20 pm https://openclipart.org/detail/263940/clock#google_vignette
+#Sound source Alarm Bell by SergeQuadrado -- https://freesound.org/s/460262/ -- License: Attribution NonCommercial 3.0
+#<a href="https://www.flaticon.com/free-icons/clock" title="clock icons">Clock icons created by Freepik - Flaticon</a>
